6dbecb443ad1ab8294854821d46f02821d65da00
Duan-Xuan <duannxph21488@fpt.edu.vn>
2023-08-07 20:34:07 +0700

    Update

--- app/src/main/java/com/example/appthu2/object.java
+++ app/src/main/java/com/example/appthu2/object.java
@@ -1,5 +1,45 @@
 package com.example.appthu2;
 
 public class object {
+    String tenTruyen, tacGia, namSanXuat, anhBia, noiDung;
 
+    public String getTenTruyen() {
+        return tenTruyen;
+    }
+
+    public void setTenTruyen(String tenTruyen) {
+        this.tenTruyen = tenTruyen;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public String getNamSanXuat() {
+        return namSanXuat;
+    }
+
+    public void setNamSanXuat(String namSanXuat) {
+        this.namSanXuat = namSanXuat;
+    }
+
+    public String getAnhBia() {
+        return anhBia;
+    }
+
+    public void setAnhBia(String anhBia) {
+        this.anhBia = anhBia;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung) {
+        this.noiDung = noiDung;
+    }
 }
